af2f76386dc92a045ffcb1f55a70bb07ee1a5292
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2023-05-31 16:12:44 +0200

    Definer metoder direkte på klientklassen (#8)
    * Definer metoder direkte på klientklassen
    
    * Bruk utvetydige exceptions
    
    * Logg ny status ved oppdatering
    
    * Fiks feil variabel i loggmelding

--- src/main/kotlin/no/nav/helsearbeidsgiver/arbeidsgivernotifikasjon/ArbeidsgiverNotifikasjonKlient.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/arbeidsgivernotifikasjon/ArbeidsgiverNotifikasjonKlient.kt
@@ -1,29 +1,223 @@
 package no.nav.helsearbeidsgiver.arbeidsgivernotifikasjon
 
 import com.expediagroup.graphql.client.ktor.GraphQLKtorClient
+import com.expediagroup.graphql.client.types.GraphQLClientError
 import com.expediagroup.graphql.client.types.GraphQLClientRequest
-import com.expediagroup.graphql.client.types.GraphQLClientResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache5.Apache5
 import io.ktor.client.request.bearerAuth
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.HardDeleteSak
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.ID
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.ISO8601DateTime
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.NyStatusSak
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.NyStatusSakByGrupperingsid
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.OppgaveUtfoert
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.OpprettNyOppgave
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.OpprettNySak
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.SoftDeleteSak
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.SoftDeleteSakByGrupperingsid
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.Whoami
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.enums.SaksStatus
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.harddeletesak.HardDeleteSakVellykket
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.nystatussak.NyStatusSakVellykket
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.oppgaveutfoert.OppgaveUtfoertVellykket
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.opprettnyoppgave.NyOppgaveVellykket
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.opprettnysak.NySakVellykket
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.softdeletesak.SoftDeleteSakVellykket
 import org.slf4j.LoggerFactory
 import java.net.URL
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.nystatussakbygrupperingsid.NyStatusSakVellykket as NyStatusSakByGrupperingsidVellykket
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.softdeletesakbygrupperingsid.SoftDeleteSakVellykket as SoftDeleteSakByGrupperingsidVellykket
 
 class ArbeidsgiverNotifikasjonKlient(
     url: String,
     private val getAccessToken: () -> String
 ) {
-    internal val logger = LoggerFactory.getLogger(this::class.java)
+    private val logger = LoggerFactory.getLogger(this::class.java)
 
     private val graphQLClient = GraphQLKtorClient(
         url = URL(url),
         httpClient = createHttpClient()
     )
 
-    internal suspend fun <T : Any> execute(query: GraphQLClientRequest<T>): GraphQLClientResponse<T> =
-        graphQLClient.execute(query) {
+    suspend fun hardDeleteSak(id: String): ID =
+        HardDeleteSak(
+            variables = HardDeleteSak.Variables(id)
+        )
+            .also { logger.info("Forsøker å slette sak $id") }
+            .execute(
+                toResult = HardDeleteSak.Result::hardDeleteSak,
+                toSuccess = { it as? HardDeleteSakVellykket },
+                onError = { res, err -> Feil.hardDeleteSak(id, res, err) }
+            )
+            .id
+            .also { logger.info("Slettet sak $it") }
+
+    suspend fun nyStatusSak(id: String, nyLenkeTilSak: String, status: SaksStatus, statusTekst: String?): ID =
+        NyStatusSak(
+            variables = NyStatusSak.Variables(
+                nyStatusSakId = id,
+                nyLenkeTilSak = nyLenkeTilSak,
+                status = status,
+                statusTekst = statusTekst
+            )
+        )
+            .also { logger.info("Forsøker å sette ny status '$status' for sak $id") }
+            .execute(
+                toResult = NyStatusSak.Result::nyStatusSak,
+                toSuccess = { it as? NyStatusSakVellykket },
+                onError = { res, err -> Feil.nyStatusSak(id, res, err) }
+            )
+            .id
+            .also { logger.info("Satt ny status '$status' for sak $it") }
+
+    suspend fun nyStatusSakByGrupperingsid(grupperingsid: String, merkelapp: String, nyStatus: SaksStatus): ID =
+        NyStatusSakByGrupperingsid(
+            variables = NyStatusSakByGrupperingsid.Variables(
+                grupperingsid = grupperingsid,
+                merkelapp = merkelapp,
+                nyStatus = nyStatus
+            )
+        )
+            .also { logger.info("Forsøker å sette ny status '$nyStatus' for grupperingsid $grupperingsid") }
+            .execute(
+                toResult = NyStatusSakByGrupperingsid.Result::nyStatusSakByGrupperingsid,
+                toSuccess = { it as? NyStatusSakByGrupperingsidVellykket },
+                onError = { res, err -> Feil.nyStatusSakByGrupperingsid(grupperingsid, nyStatus, res, err) }
+            )
+            .id
+            .also { logger.info("Satt ny status '$nyStatus' for sak $it") }
+
+    suspend fun oppgaveUtfoert(id: String): ID =
+        OppgaveUtfoert(
+            variables = OppgaveUtfoert.Variables(id)
+        )
+            .also { logger.info("Forsøker å opprette ny sak mot arbeidsgiver-notifikasjoner") }
+            .execute(
+                toResult = OppgaveUtfoert.Result::oppgaveUtfoert,
+                toSuccess = { it as? OppgaveUtfoertVellykket },
+                onError = { res, err -> Feil.oppgaveUtfoert(id, res, err) }
+            )
+            .id
+            .also { logger.info("Oppgave utført $it") }
+
+    suspend fun opprettNySak(
+        grupperingsid: String,
+        merkelapp: String,
+        virksomhetsnummer: String,
+        tittel: String,
+        lenke: String,
+        statusTekst: String?,
+        harddeleteOm: String
+    ): ID =
+        OpprettNySak(
+            variables = OpprettNySak.Variables(
+                grupperingsid = grupperingsid,
+                merkelapp = merkelapp,
+                virksomhetsnummer = virksomhetsnummer,
+                tittel = tittel,
+                lenke = lenke,
+                statusTekst = statusTekst,
+                harddeleteOm = harddeleteOm
+            )
+        )
+            .also { logger.info("Forsøker å opprette ny sak mot arbeidsgiver-notifikasjoner") }
+            .execute(
+                toResult = OpprettNySak.Result::nySak,
+                toSuccess = { it as? NySakVellykket },
+                onError = Feil::opprettNySak
+            )
+            .id
+            .also { logger.info("Opprettet ny sak $it") }
+
+    suspend fun opprettNyOppgave(
+        eksternId: String,
+        lenke: String,
+        tekst: String,
+        virksomhetsnummer: String,
+        merkelapp: String,
+        tidspunkt: ISO8601DateTime?,
+        grupperingsid: String?,
+        varslingTittel: String,
+        varslingInnhold: String
+    ): ID =
+        OpprettNyOppgave(
+            variables = OpprettNyOppgave.Variables(
+                eksternId = eksternId,
+                lenke = lenke,
+                tekst = tekst,
+                virksomhetsnummer = virksomhetsnummer,
+                merkelapp = merkelapp,
+                tidspunkt = tidspunkt,
+                grupperingsid = grupperingsid,
+                varslingTittel = varslingTittel,
+                varslingInnhold = varslingInnhold
+            )
+        )
+            .also { logger.info("Forsøker å opprette ny oppgave mot arbeidsgiver-notifikasjoner") }
+            .execute(
+                toResult = OpprettNyOppgave.Result::nyOppgave,
+                toSuccess = { it as? NyOppgaveVellykket },
+                onError = Feil::nyOppgave
+            )
+            .id
+            .also { logger.info("Opprettet ny oppgave med id: $it") }
+
+    suspend fun softDeleteSak(id: String): ID =
+        SoftDeleteSak(
+            variables = SoftDeleteSak.Variables(id)
+        )
+            .also { logger.info("Forsøker å slette sak $id") }
+            .execute(
+                toResult = SoftDeleteSak.Result::softDeleteSak,
+                toSuccess = { it as? SoftDeleteSakVellykket },
+                onError = { res, err -> Feil.softDeleteSak(id, res, err) }
+            )
+            .id
+            .also { logger.info("Slettet sak $it") }
+
+    suspend fun softDeleteSakByGrupperingsid(grupperingsid: String, merkelapp: String): ID =
+        SoftDeleteSakByGrupperingsid(
+            variables = SoftDeleteSakByGrupperingsid.Variables(
+                grupperingsid = grupperingsid,
+                merkelapp = merkelapp
+            )
+        )
+            .also { logger.info("Forsøker å slette sak med grupperingsid $grupperingsid og merkelapp $merkelapp") }
+            .execute(
+                toResult = SoftDeleteSakByGrupperingsid.Result::softDeleteSakByGrupperingsid,
+                toSuccess = { it as? SoftDeleteSakByGrupperingsidVellykket },
+                onError = { res, err -> Feil.softDeleteSakByGrupperingsid(grupperingsid, res, err) }
+            )
+            .id
+            .also { logger.info("Slettet sak $it") }
+
+    suspend fun whoami(): String? =
+        Whoami()
+            .also { logger.info("Henter 'whoami' info fra arbeidsgiver-notifikasjon-api") }
+            .execute(
+                toResult = { this },
+                toSuccess = { it },
+                onError = { _, _ -> throw RuntimeException("Feil ved henting av 'whoami'") }
+            )
+            .whoami
+            .also { logger.info("Whoami: $it") }
+
+    private suspend fun <Data : Any, Result : Any, Success : Result> GraphQLClientRequest<Data>.execute(
+        toResult: Data.() -> Result,
+        toSuccess: (Result?) -> Success?,
+        onError: (Result?, List<GraphQLClientError>?) -> Nothing
+    ): Success {
+        val response = graphQLClient.execute(this) {
             bearerAuth(getAccessToken())
         }
+
+        val result = response.data?.toResult()
+
+        return runCatching { toSuccess(result) }
+            .getOrNull()
+            ?: onError(result, response.errors)
+    }
 }
 
 internal fun createHttpClient(): HttpClient =
---
+++ src/main/kotlin/no/nav/helsearbeidsgiver/arbeidsgivernotifikasjon/Feil.kt
@@ -0,0 +1,221 @@
+package no.nav.helsearbeidsgiver.arbeidsgivernotifikasjon
+
+import com.expediagroup.graphql.client.types.GraphQLClientError
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.enums.SaksStatus
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.harddeletesak.HardDeleteSakResultat
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.nystatussak.NyStatusSakResultat
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.oppgaveutfoert.OppgaveUtfoertResultat
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.opprettnyoppgave.NyOppgaveResultat
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.opprettnysak.NySakResultat
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.softdeletesak.SoftDeleteSakResultat
+import org.slf4j.LoggerFactory
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.harddeletesak.SakFinnesIkke as FinnesIkkeHardDeleteSak
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.harddeletesak.UgyldigMerkelapp as UgyldigMerkelappHardDeleteSak
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.harddeletesak.UkjentProdusent as UkjentProdusentHardDeleteSak
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.nystatussak.Konflikt as KonfliktNyStatusSak
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.nystatussak.SakFinnesIkke as FinnesIkkeNyStatusSak
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.nystatussakbygrupperingsid.Konflikt as KonfliktNyStatusSakByGrupperingsid
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.nystatussakbygrupperingsid.NyStatusSakResultat as NyStatusSakByGrupperingsidResultat
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.nystatussakbygrupperingsid.SakFinnesIkke as SakFinnesIkkeNyStatusSakByGrupperingsid
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.oppgaveutfoert.NotifikasjonFinnesIkke as NotifikasjonFinnesIkkeOppgaveUtfoert
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.oppgaveutfoert.UgyldigMerkelapp as UgyldigMerkelappOppgaveUtfoert
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.oppgaveutfoert.UkjentProdusent as UkjentProdusentOppgaveUtfoert
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.opprettnyoppgave.DuplikatEksternIdOgMerkelapp as DuplikatEksternIdOgMerkelappNyOppgave
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.opprettnyoppgave.UgyldigMerkelapp as UgyldigMerkelappNyOppgave
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.opprettnyoppgave.UgyldigMottaker as UgyldigMottakerNyOppgave
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.opprettnyoppgave.UkjentProdusent as UkjentProdusentNyOppgave
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.opprettnyoppgave.UkjentRolle as UkjentRolleNyOppgave
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.opprettnysak.DuplikatGrupperingsid as DuplikatGrupperingsidNySak
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.opprettnysak.UgyldigMerkelapp as UgyldigMerkelappNySak
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.opprettnysak.UgyldigMottaker as UgyldigMottakerNySak
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.opprettnysak.UkjentProdusent as UkjentProdusentNySak
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.opprettnysak.UkjentRolle as UkjentRolleNySak
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.softdeletesak.SakFinnesIkke as SakFinnesIkkeSoftDeleteSak
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.softdeletesak.UgyldigMerkelapp as UgyldigMerkelappSoftDeleteSak
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.softdeletesak.UkjentProdusent as UkjentProdusentSoftDeleteSak
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.softdeletesakbygrupperingsid.SakFinnesIkke as SakFinnesIkkeSoftDeleteSakByGrupperingsid
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.softdeletesakbygrupperingsid.SoftDeleteSakResultat as SoftDeleteSakByGrupperingsidResultat
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.softdeletesakbygrupperingsid.UgyldigMerkelapp as UgyldigMerkelappSoftDeleteSakByGrupperingsid
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.softdeletesakbygrupperingsid.UkjentProdusent as UkjentProdusentSoftDeleteSakByGrupperingsid
+
+internal object Feil {
+    private val logger = LoggerFactory.getLogger(ArbeidsgiverNotifikasjonKlient::class.java)
+
+    fun hardDeleteSak(id: String, resultat: HardDeleteSakResultat?, feil: List<GraphQLClientError>?): Nothing {
+        val feilmelding = when (resultat) {
+            is UgyldigMerkelappHardDeleteSak -> resultat.feilmelding
+            is FinnesIkkeHardDeleteSak -> resultat.feilmelding
+            is UkjentProdusentHardDeleteSak -> resultat.feilmelding
+            else -> null
+        }
+
+        if (feilmelding != null) {
+            feilmelding.loggFeil()
+            throw HardDeleteSakException(id, feilmelding)
+        } else {
+            logger.error("Kunne ikke harddelete sak $resultat: med feil $feil")
+            throw HardDeleteSakException(id, feil.ukjentFeil())
+        }
+    }
+
+    fun nyStatusSak(id: String, resultat: NyStatusSakResultat?, feil: List<GraphQLClientError>?): Nothing {
+        val feilmelding = when (resultat) {
+            is FinnesIkkeNyStatusSak -> resultat.feilmelding
+            is KonfliktNyStatusSak -> resultat.feilmelding
+            else -> null
+        }
+
+        if (feilmelding != null) {
+            feilmelding.loggFeil()
+            throw NyStatusSakException(id, feilmelding)
+        } else {
+            logger.error("Kunne ikke opprette ny sak: $feil")
+            throw NyStatusSakException(id, feil.ukjentFeil())
+        }
+    }
+
+    fun nyStatusSakByGrupperingsid(
+        grupperingsid: String,
+        nyStatus: SaksStatus,
+        resultat: NyStatusSakByGrupperingsidResultat?,
+        feil: List<GraphQLClientError>?
+    ): Nothing {
+        val feilmelding = when (resultat) {
+            is SakFinnesIkkeNyStatusSakByGrupperingsid -> resultat.feilmelding
+            is KonfliktNyStatusSakByGrupperingsid -> resultat.feilmelding
+            else -> null
+        }
+
+        if (feilmelding != null) {
+            feilmelding.loggFeil()
+            throw NyStatusSakByGrupperingsidException(grupperingsid, nyStatus, feilmelding)
+        } else {
+            logger.error("Kunne ikke opprette ny sak (fra grupperingsid): $feil")
+            throw NyStatusSakByGrupperingsidException(grupperingsid, nyStatus, feil.ukjentFeil())
+        }
+    }
+
+    fun oppgaveUtfoert(id: String, resultat: OppgaveUtfoertResultat?, feil: List<GraphQLClientError>?): Nothing {
+        val feilmelding = when (resultat) {
+            is UkjentProdusentOppgaveUtfoert -> resultat.feilmelding
+            is UgyldigMerkelappOppgaveUtfoert -> resultat.feilmelding
+            is NotifikasjonFinnesIkkeOppgaveUtfoert -> resultat.feilmelding
+            else -> null
+        }
+
+        if (feilmelding != null) {
+            feilmelding.loggFeil()
+            throw OppgaveUtfoertException(id, feilmelding)
+        } else {
+            logger.error("Kunne ikke opprette utføre oppgave: $feil")
+            throw OppgaveUtfoertException(id, feil.ukjentFeil())
+        }
+    }
+
+    fun opprettNySak(resultat: NySakResultat?, feil: List<GraphQLClientError>?): Nothing {
+        val feilmelding = when (resultat) {
+            is UgyldigMerkelappNySak -> resultat.feilmelding
+            is UgyldigMottakerNySak -> resultat.feilmelding
+            is UkjentProdusentNySak -> resultat.feilmelding
+            is UkjentRolleNySak -> resultat.feilmelding
+            is DuplikatGrupperingsidNySak -> resultat.feilmelding
+            else -> null
+        }
+
+        if (feilmelding != null) {
+            feilmelding.loggFeil()
+            throw OpprettNySakException(feilmelding)
+        } else {
+            logger.error("Kunne ikke opprette ny sak: $feil")
+            throw OpprettNySakException(feil.ukjentFeil())
+        }
+    }
+
+    fun nyOppgave(resultat: NyOppgaveResultat?, feil: List<GraphQLClientError>?): Nothing {
+        val feilmelding = when (resultat) {
+            is UgyldigMottakerNyOppgave -> resultat.feilmelding
+            is UkjentRolleNyOppgave -> resultat.feilmelding
+            is UgyldigMerkelappNyOppgave -> resultat.feilmelding
+            is UkjentProdusentNyOppgave -> resultat.feilmelding
+            is DuplikatEksternIdOgMerkelappNyOppgave -> resultat.feilmelding
+            else -> null
+        }
+
+        if (feilmelding != null) {
+            feilmelding.loggFeil()
+            throw OpprettNyOppgaveException(feilmelding)
+        } else {
+            logger.error("Kunne ikke opprette ny oppgave: $feil")
+            throw OpprettNyOppgaveException(feil.ukjentFeil())
+        }
+    }
+
+    fun softDeleteSak(id: String, resultat: SoftDeleteSakResultat?, feil: List<GraphQLClientError>?): Nothing {
+        val feilmelding = when (resultat) {
+            is UgyldigMerkelappSoftDeleteSak -> resultat.feilmelding
+            is SakFinnesIkkeSoftDeleteSak -> resultat.feilmelding
+            is UkjentProdusentSoftDeleteSak -> resultat.feilmelding
+            else -> null
+        }
+
+        if (feilmelding != null) {
+            feilmelding.loggFeil()
+            throw SoftDeleteSakException(id, feilmelding)
+        } else {
+            logger.error("Kunne ikke softdelete sak: $feil")
+            throw SoftDeleteSakException(id, feil.ukjentFeil())
+        }
+    }
+
+    fun softDeleteSakByGrupperingsid(
+        grupperingsid: String,
+        resultat: SoftDeleteSakByGrupperingsidResultat?,
+        feil: List<GraphQLClientError>?
+    ): Nothing {
+        val feilmelding = when (resultat) {
+            is UgyldigMerkelappSoftDeleteSakByGrupperingsid -> resultat.feilmelding
+            is SakFinnesIkkeSoftDeleteSakByGrupperingsid -> resultat.feilmelding
+            is UkjentProdusentSoftDeleteSakByGrupperingsid -> resultat.feilmelding
+            else -> null
+        }
+
+        if (feilmelding != null) {
+            feilmelding.loggFeil()
+            throw SoftDeleteSakByGrupperingsidException(grupperingsid, feilmelding)
+        } else {
+            logger.error("Kunne ikke softdelete sak (fra grupperingsid): $feil")
+            throw SoftDeleteSakByGrupperingsidException(grupperingsid, feil.ukjentFeil())
+        }
+    }
+
+    private fun String.loggFeil() {
+        logger.error("Feilmelding $this")
+    }
+
+    private fun List<GraphQLClientError>?.ukjentFeil(): String =
+        "ukjent feil: $this"
+}
+
+class HardDeleteSakException(id: String, feilmelding: String?) :
+    Exception("Sletting av sak $id mot arbeidsgiver-notifikasjon-api feilet: $feilmelding")
+
+class NyStatusSakException(id: String, feilmelding: String?) :
+    Exception("Ny status for sak $id arbeidsgiver-notifikasjon-api feilet med: $feilmelding")
+
+class NyStatusSakByGrupperingsidException(grupperingsid: String, status: SaksStatus, feilmelding: String?) :
+    Exception("Ny status $status for sak $grupperingsid arbeidsgiver-notifikasjon-api feilet med: $feilmelding")
+
+class OppgaveUtfoertException(id: String, feilmelding: String?) :
+    Exception("Utføring av oppgave $id mot arbeidsgiver-notifikasjon-api feilet: $feilmelding")
+
+class OpprettNySakException(feilmelding: String?) :
+    Exception("Opprettelse av ny sak mot arbeidsgiver-notifikasjon-api feilet: $feilmelding")
+
+class OpprettNyOppgaveException(feilmelding: String?) :
+    Exception("Opprettelse av ny oppgave mot arbeidsgiver-notifikasjon-api feilet: $feilmelding")
+
+class SoftDeleteSakException(id: String, feilmelding: String?) :
+    Exception("Sletting av sak $id mot arbeidsgiver-notifikasjon-api feilet: $feilmelding")
+
+class SoftDeleteSakByGrupperingsidException(grupperingsid: String, feilmelding: String?) :
+    Exception("Sletting av sak $grupperingsid mot arbeidsgiver-notifikasjon-api feilet: $feilmelding")

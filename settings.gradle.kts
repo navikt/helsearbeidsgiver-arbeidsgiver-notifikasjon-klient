97845957894caee4843dd93317ccdf8ffda14200
Mikael Bjerga <mikael.bjerga@nav.no>
2025-10-07 14:15:00 +0200

    Bump avhengigheter

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,12 +3,14 @@ rootProject.name = "arbeidsgiver-notifikasjon-klient"
 pluginManagement {
     plugins {
         val graphQLKotlinVersion: String by settings
+        val kotestVersion: String by settings
         val kotlinVersion: String by settings
         val kotlinterVersion: String by settings
 
         kotlin("jvm") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion
         id("com.expediagroup.graphql") version graphQLKotlinVersion
+        id("io.kotest") version kotestVersion
         id("org.jmailen.kotlinter") version kotlinterVersion
     }
 }
